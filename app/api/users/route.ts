f5f4f107b522b522e7d7f400e7a071224b62c548
Sanam Gautam <sanam_gautam@tokma.ai>
2025-12-09 16:43:34 +0545

    user api setup

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,24 +3,12 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const users = await prisma.user.findMany({
-    include: { posts: true },
   });
-
-  return NextResponse.json(users);
-}
-
-export async function POST(req: Request) {
-  const body = await req.json();
-  const { email, name } = body;
-  console.log("Received signup data:", body);
-
-  if (!email) {
-    return NextResponse.json({ error: "Email is required" }, { status: 400 });
-  }
-
-  const user = await prisma.user.create({
-    data: { email, name },
-  });
-
-  return NextResponse.json(user, { status: 201 });
+  const data = users.map(u => ({
+    id: u.id,
+    firstName: u.firstName,
+    lastName: u.lastName,
+    email: u.email
+  }))
+  return NextResponse.json({ data, message: "Success!" }, { status: 200 });
 }
