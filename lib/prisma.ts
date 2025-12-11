7c23027a3054396690c574998a37171ca14fb54c
abhishakepoudel <poudelabhishake304@gmail.com>
2025-12-11 13:43:49 +0545

    fix prisma error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from "@prisma/client";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/app/generated/prisma/client";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -20,3 +20,4 @@ export const prisma =
   });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
