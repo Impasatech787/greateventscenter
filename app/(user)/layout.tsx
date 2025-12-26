629aae8c860bfe6f02fade9c0a83ed8a745a4849
abhishakepoudel <poudelabhishake304@gmail.com>
2025-12-26 10:27:03 +0545

    feat build errors

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,7 +1,6 @@
 import "@/app/globals.css";
 import Footer from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
-import { RoleProvider } from "../context/AuthContext";
 
 export default function RootLayout({
   children,
@@ -9,10 +8,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <RoleProvider>
+    <>
       <Navbar />
       {children}
       <Footer />
-    </RoleProvider>
+    </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Manrope } from "next/font/google";
 import "./globals.css";
+import { RoleProvider } from "./context/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${manRope.variable} antialiased`}
       >
-        {children}
+        <RoleProvider>{children}</RoleProvider>
       </body>
     </html>
   );
